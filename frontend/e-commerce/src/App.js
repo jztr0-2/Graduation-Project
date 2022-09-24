28413a61c394a4d860973ea7673f4146e7c193c3
Phan Tan Loc <lcphan512@gmail.com>
2022-09-24 09:41:48 +0700

    Config router, build base default layout, single layout

--- frontend/e-commerce/src/App.js
+++ frontend/e-commerce/src/App.js
@@ -1,10 +1,37 @@
-import GlobalStyles from './components/GlobalStyles';
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DefaultLayout } from '~/components/Layout';
 function App() {
     return (
-        <GlobalStyles>
-            <div className="App">Hello React</div>
-        </GlobalStyles>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
