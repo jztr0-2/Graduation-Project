646cfac270a8aafa445f50f0c4a4560af886bbaa
xxiihdc <tran.van.hoai.duc@sun-asterisk.com>
2022-12-20 19:06:15 +0700

    update import file data

--- backend/e-commerce/src/main/java/com/poly/jztr/ecommerce/controller/publics/UsersController.java
+++ backend/e-commerce/src/main/java/com/poly/jztr/ecommerce/controller/publics/UsersController.java
@@ -8,6 +8,9 @@ import com.poly.jztr.ecommerce.dto.UserDto;
 import com.poly.jztr.ecommerce.expection.DuplicateEntryException;
 import com.poly.jztr.ecommerce.model.*;
 import com.poly.jztr.ecommerce.service.*;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,9 +19,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.apache.commons.lang3.RandomStringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 
-import java.nio.charset.StandardCharsets;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -266,5 +277,141 @@ public class UsersController {
             return "DONE";
         }
         return "FAIL";
+    }
+
+    @PostMapping("init-file-data")
+    public String initFileData(@RequestParam MultipartFile file) throws IOException {
+        XSSFWorkbook workbook = new XSSFWorkbook(file.getInputStream());
+        XSSFSheet categorySheet = workbook.getSheetAt(0);
+        for(int i=1;i<categorySheet.getPhysicalNumberOfRows() ;i++) {
+            XSSFRow row = categorySheet.getRow(i);
+            Category category = new Category();
+            category.setName(row.getCell(0).toString().trim());
+            categoryService.save(category);
+        }
+
+        XSSFSheet userSheet = workbook.getSheetAt(1);
+
+        for(int i=1;i<99;i++) {
+            XSSFRow row = userSheet.getRow(i);
+            String fullname = row.getCell(0)+"";
+                User user = new User();
+                user.setFirstName(fullname.substring(0,fullname.lastIndexOf(" ")).trim());
+                user.setLastName(fullname.substring(fullname.lastIndexOf(" ")).trim());
+                user.setEmail(row.getCell(1).toString().trim());
+                user.setPhone(row.getCell(2).toString().trim());
+                user.setPassword(passwordEncoder.encode("123456A@a"));
+                user.setStatus(Constant.USER_STATUS_ACTIVATED);
+                service.save(user);
+        }
+
+        XSSFSheet addressSheet = workbook.getSheetAt(2);
+
+        for(int i=1;i<addressSheet.getPhysicalNumberOfRows() ;i++) {
+            XSSFRow row = addressSheet.getRow(i);
+            Address address = new Address();
+            address.setProvince(row.getCell(0).toString().trim());
+            address.setDistrict(row.getCell(1).toString().trim());
+            address.setCommune(row.getCell(2).toString().trim());
+            address.setWard(row.getCell(3).toString().trim());
+            address.setAppartmentNo(row.getCell(4).toString().trim());
+            address.setPhone(row.getCell(5).toString().trim());
+            addressService.save(address);
+        }
+
+        XSSFSheet productSheet = workbook.getSheetAt(3);
+
+        String title  = "";
+        Random random = new Random();
+        Product product = new Product();
+        for(int i=1;i<productSheet.getPhysicalNumberOfRows() ;i++) {
+            XSSFRow row = productSheet.getRow(i);
+            if(i >= 212) break;
+            String productName = row.getCell(0)+"".trim();
+            if(productName != "" && productName != "null" && !productName.isEmpty() && !productName.isBlank() && productName != null){
+                product = new Product();
+                product.setName(productName);
+                Category category = new Category(random.nextLong(1,9));//categoryService.findByName(row.getCell(1).toString().trim()).get();
+                product.setCategory(category);
+                product.setDescription(row.getCell(2).toString().trim());
+                title = row.getCell(3).toString().trim();
+                product.setStatus(0);
+                product = productService.save(product);
+                String imgLink = row.getCell(8).toString().trim();
+                System.out.println(imgLink);
+                System.out.println(row.getCell(7).toString().trim());
+                Image img = saveImg(imgLink,"product_avt" + product.getId() +  random.nextInt() + ".jpg",Constant.IMAGE_TYPE_PRODUCT_AVT,0L);
+                product.setImage(img);
+                productService.save(product);
+            }else{
+                ProductVariant variant = new ProductVariant();
+                variant.setProduct(product);
+                variant.setQuantity(10000);
+                System.out.println(row.getCell(4));
+                variant.setUnitPrice(row.getCell(4).getNumericCellValue());
+                variant.setDescription("{\"title\": \""+title+"\", \""+title+"\": "+row.getCell(7)+"".trim()+"\"}");
+                String imgLink = row.getCell(8).toString().trim();
+                saveImg(imgLink,"product_lst" + product.getId() + random.nextInt() + ".jpg",Constant.IMAGE_TYPE_PRODUCT_MULTIPLE,product.getId());
+                variant.setDisplayName(productName);
+                productVariantService.save(variant);
+            }
+        }
+
+        // init order
+
+        for(int i = 0; i < 1000; i ++){
+            Address address = new Address(random.nextLong(1,19));
+            User user= service.findById(random.nextLong(90) + 1).get();
+            Order order = new Order();
+            order.setUser(user);
+            order.setStatus(Constant.ORDER_STATUS_SUCCESS);
+            order.setDescription("Fake order" + i);
+
+
+            String month = ((i%12) + 1) +"";
+
+            month = month.length() == 1 ? "0" + month : month;
+            String stringDate = "09:15:30 PM, "+month+"/09/2022";
+            String pattern = "hh:mm:ss a, M/d/uuuu";
+            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(pattern, Locale.US);
+            LocalDateTime localDateTime = LocalDateTime.parse(stringDate, dateTimeFormatter);
+            ZoneId zoneId = ZoneId.of("America/Chicago");
+            ZonedDateTime zonedDateTime = localDateTime.atZone(zoneId);
+            Instant instant = zonedDateTime.toInstant();
+
+
+            order.setCreatedAt(instant);
+            order.setCreatedAt(instant);
+            List<OrderItem> orderItemList = new ArrayList<>();
+            order.setAddress(address);
+            for(int j = 0; j < 5; j ++){
+                OrderItem orderItem = new OrderItem();
+                orderItem.setOrder(order);
+                orderItem.setQuantity(random.nextInt(10));
+                orderItem.setUnitPrice(random.nextDouble(1500));
+                orderItem.setProductVariant(new ProductVariant(random.nextLong(1,137));
+                orderItemList.add(orderItem);
+            }
+            order.setOrderItems(orderItemList);
+            orderService.save(order);
+        }
+        return "OK";
+    }
+
+    @Autowired ImageService imageService;
+
+    private Image saveImg(String surl,String fileName, Integer type, Long id) throws IOException {
+        URL url = new URL(surl);
+        InputStream in = new BufferedInputStream(url.openStream());
+        Path path= Paths.get("uploads");
+        Files.copy(in,path.resolve(fileName), StandardCopyOption.REPLACE_EXISTING);
+        Image image = new Image();
+        image.setTitle(fileName);
+        image.setType(type);
+        if (id != 0L){
+            image.setProductId(id);
+        }
+        return imageService.save(image);
+
     }
 }
