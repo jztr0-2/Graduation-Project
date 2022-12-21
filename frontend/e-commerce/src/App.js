db282c9aa8c0297fcd201c644640e1bd8c8152f9
Tran An Thuyen <thuyen8tui@gmail.com>
2022-12-21 21:54:36 +0700

    login

--- frontend/e-commerce/src/App.js
+++ frontend/e-commerce/src/App.js
@@ -3,6 +3,25 @@ import { BrowserRouter as Router, Routes, Route, Form } from 'react-router-dom';
 import { publicRoutes } from '~/routes';
 import { DefaultLayout } from '~/components/Layout';
 function App() {
+    // useEffect(() => {
+    //     isAdmin().then((res) => {
+    //         if (res.statusCode == 401) {
+    //             setToken(false);
+    //         } else {
+    //             setToken(true);
+    //         }
+    //     });
+    // }, []);
+
+    // const [token, setToken] = useState();
+    // if (!token) {
+    //     return <LoginForm setToken={setToken} />;
+    // }
+    // const logout = () => {
+    //     localStorage.clear();
+    //     alert('Logout succsecfuly');
+    //     window.location.reload(false);
+    // };
     return (
         <Router>
             <div className="App">
