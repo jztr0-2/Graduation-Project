971b4bdf01e0f8a08fb62b5a4d24fbd89d957db9
LanHuong142002 <lanhuong142002@gmail.com>
2022-12-21 15:42:10 +0700

    dont

---
+++ frontend/e-commerce/src/pages/Contact/index.js
@@ -0,0 +1,13 @@
+import classNames from 'classnames/bind';
+import styles from './Contact.module.scss';
+const cx = classNames.bind(styles);
+
+function Contact() {
+    return (
+        <div className={cx('container-others')}>
+            <h1>asdasd</h1>
+        </div>
+    );
+}
+
+export default Contact;
